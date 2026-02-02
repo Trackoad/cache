cb6d1af0d00382d0296bbd321610523a2e80b032
Cyril Besse <cbesse@spordle.com>
2026-02-02 14:14:05 -0500

    feat: add logging middleware to track incoming requests

--- src/index.mjs
+++ src/index.mjs
@@ -5,12 +5,16 @@ config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('in', req.originalUrl);
+  next();
+});
+
 app.get('/', (req, res) => {
 
   res.header('CDN-Cache-Control', `max-age=${15}`);
   res.header('Cache-Control', `max-age=${15}`);
 
-  console.log(req.originalUrl, 'data');
   res.json({ ok: true });
 });
 
